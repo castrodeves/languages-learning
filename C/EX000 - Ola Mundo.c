2451ce19c2ed3e587dea7f238b4361c562f8d48c
Eduardo Castro <tec.eduardocastro@gmail.com>
2021-02-10 11:37:35 -0300

    First programs in c

---
+++ C/EX000 - Ola Mundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+    setlocale(LC_ALL, "Portuguese");
+    printf("Olá, mundo!\nUfa! Já estou \"LIVRE\" da maldição!\n");
+}
---
+++ C/EX002 - Especiais.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    printf(
+        "<<< EX002 - Specials >>>\n"
+        "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+        "\n \\n \t=\t New Line"
+        "\n \\t \t=\t Tab"
+        "\n \\b \t=\t Backspace"
+        "\n \\r \t=\t Return"
+        "\n \\\\ \t=\t Backslash"
+        "\n \\\" \t=\t Quotation marks"
+        "\n \\? \t=\t Interrogation"
+        "\n \\a \t=\t Beep"
+        "\n %%%% \t=\t Percentage"
+        "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
+    );
+}
