77ede43650fd8713dd988d857948abe78441b71e
Eduardo Castro <tec.eduardocastro@gmail.com>
2022-06-05 22:41:36 -0300

    JavaFX switch scenes

--- Java/javafx/src/AppController.java
+++ Java/javafx/src/AppController.java
@@ -1,10 +1,37 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class AppController {
+  private Stage stage;
+  // private Parent root;
+  private Scene scene;
+
+  public void switchToLogin(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/App.fxml"));
+    stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+  
+  public void switchToHome(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/Home.fxml"));
+    stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
   @FXML
   private TextField username;
 
@@ -12,12 +39,18 @@ public class AppController {
   private PasswordField userpassword;
 
   @FXML
-  void login(ActionEvent event) {
+  void login(ActionEvent event) throws IOException {
     String user = username.getText();
     String pass = userpassword.getText();
 
     if (user.equals("admin") && pass.equals("admin")) {
       System.out.println("Login Successful");
+      try {
+        switchToHome(event);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      
     } else {
       System.out.println("Login Failed");
     }
