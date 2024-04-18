38437f581c37ebefbe0f65d85178a7201ea46383
Eduardo Castro <tec.eduardocasto@gmail.com>
2024-04-17 22:56:19 -0300

    feat: add course erick wendel fundamentos javascript

---
+++ languages/javascript/ew-fundamentos-javascript/03-tipos-de-dados-pt-02/demo-04-comentarios.js
@@ -0,0 +1,11 @@
+// comentário em uma unica linha
+
+/**
+ * Esse código é Hackerzão
+ * Author: thecastrodev
+ * Data: 17/04/2024
+ */
+
+// ignorando a linha abaixo
+// const variavelConstante = 1+2+4+4 
+const variavelConstante = 1 + 2 + 4 + 4 // na mesma linha
